eac431b1c8f8d4effa9f7bb542b4c2e90ffc14bd
Eric Soto <easoto@iss-pr.com>
2024-09-06 23:17:42 -0400

    Tweak to remove the hardcoded builder name and use a builder name defined in build-manifest.env.

--- x_build.sh
+++ x_build.sh
@@ -3,6 +3,12 @@ x#!/bin/bash
 # Load environment variables from build-manifest.env
 source build-manifest.env
 
+# verify that we have the necessary environment variables
+if [ -z "$BUILDER_NAME" ] || [ -z "$NAME" ] || [ -z "$CURR_TAG" ]; then
+  echo "BUILDER_NAME, NAME and CURR_TAG must be set in build-manifest.env"
+  exit 1
+fi
+
 # ask if we want to build the image for multi platform or current platform only
 echo "Do you want to build the image for multi platform or current platform only?"
 echo "1. Multi platform (Requires docker buildx support qith qemu - built into macOS Docker Desktop)"
@@ -11,7 +17,6 @@ read -p "Enter your choice: " choice
 case $choice in
   1)
     echo "Building the image for multi platform"
-    BUILDER_NAME="ericbuilder"
     # Check if the builder exists
     if docker buildx inspect "$BUILDER_NAME" > /dev/null 2>&1; then
       echo "Builder $BUILDER_NAME already exists. Reusing it."
